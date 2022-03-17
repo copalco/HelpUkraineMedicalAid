00cf8d2b17bbe7ee7b4671b232d6252c751a6b31
ylytvyn <ylytvyn@morebis.pl>
2022-03-17 10:44:33 +0200

    Nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./modules/material.module";
 import {HttpClientModule, HttpClient} from '@angular/common/http'
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core'
-import {TranslateHttpLoader} from '@ngx-translate/http-loader'
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { NavigationComponent } from './components/navigation/navigation.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSelectModule} from "@angular/material/select";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
 
 const MATERIAL = [
   MatToolbarModule,
   MatIconModule,
-  MatSelectModule
+  MatSelectModule,
+  MatButtonToggleModule
 ];
 
 @NgModule({
